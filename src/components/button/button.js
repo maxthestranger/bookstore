e50dabc34ebdc62d40d2a4a3bf0da06fe573135c
maxthestranger <thestrangermax@gmail.com>
2022-04-28 14:30:14 +0300

    fix linter errors

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,15 @@
+import { PropTypes } from 'prop-types';
 import styles from './button.module.scss';
 
-const Button = ({ type, title }) => {
-  return (
-    <button type={type} className={styles.btn}>
-      {title}
-    </button>
-  );
+const Button = ({ type, title }) => (
+  <button type={type === 'submit' ? 'submit' : 'button'} className={styles.btn}>
+    {title}
+  </button>
+);
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Button;
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,15 +1,13 @@
 import Button from '../components/button/button';
 import Header from '../components/header/header';
 
-const Categories = () => {
-  return (
-    <>
-      <Header />
-      <div className="container mt-5">
-        <Button type="button" title="Check status" />
-      </div>
-    </>
-  );
-};
+const Categories = () => (
+  <>
+    <Header />
+    <div className="container mt-5">
+      <Button type="button" title="Check status" />
+    </div>
+  </>
+);
 
 export default Categories;
